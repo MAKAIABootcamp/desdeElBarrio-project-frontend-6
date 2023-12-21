9538b200753c565344d305af3fcdf308e97804c9
veroa8 <veronica.tangarife1@udea.edu.co>
2023-12-20 22:31:03 -0500

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div><h1>Demo Day</h1></div>
+  </React.StrictMode>,
+)

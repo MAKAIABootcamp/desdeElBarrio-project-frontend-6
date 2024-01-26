bf7707464e0630c9a88ad2cf574f75b1048e1351
Zamir Flores Cordoba <zflorez@miuniclaretiana.edu.co>
2024-01-25 23:57:34 -0500

    complete

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-
+import { useSelector, useDispatch } from "react-redux";
 import Home from "../pages/home/Home";
 import NavBar from "../components/NavBar/NavBar";
 import Apoyanos from "../pages/apoyanos/Apoyanos";
@@ -9,36 +9,81 @@ import Eventos from "../pages/novedades/Eventos";
 import Talleres from "../pages/novedades/Talleres";
 import Noticias from "../pages/novedades/Noticias";
 import Register from "../pages/Register/register";
-import History from "../pages/history/History";
-import AdminPanel from "../pages/adminPanel/AdminPanel";
-import Contacto from "../pages/contact/Contacto";
-import { useAuth } from "../context/AuthContext";
+import Contacto from "../pages/contact/Contacto"
+// import { useAuth } from "../context/AuthContext";
 import PublicRoutes from "./PublicRoutes";
 import PrivatedRoutes from "./PrivatedRoutes";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { setIsAuthenticate, setUser } from "../store/users/userSlice";
+import { auth } from "../firebase/firebase.config";
+import { getUserFromFirestoreAction } from "../store/users/userThunks";
+import History from "../pages/history/History";
+import AdminPanel from "../pages/adminPanel/AdminPanel";
+import { useAuth } from "../context/AuthContext";
 import Slider from "../components/Slider/Slider";
 
 export const NavRoutes = () => {
-  const auth = useAuth();
+  // const auth = useAuth();
+  const { isAuthenticate, user } = useSelector((store) => store.user);
+  const [checking, setChecking] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userLogged) => {
+      if (userLogged?.uid && !user) {
+        dispatch(getUserFromFirestoreAction(userLogged.uid));
+        // dispatch(setIsAuthenticate(true));
+
+        // dispatch(
+        //   setUser({
+        //     id: userLogged.uid,
+        //     email: userLogged.email,
+        //     name: userLogged.displayName,
+        //     photoURL: userLogged.photoURL,
+        //     accessToken: userLogged.accessToken,
+        //   })
+        // );
+        // dispatch(setError(false))
+      }
+    });
+
+    setChecking(false);
+  }, [dispatch, user]);
+
+  if (checking) {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <>
       <Routes>
         <Route element={<NavBar />}>
-          <Route index element={<Home />} />
-          <Route element={<PublicRoutes isAuthenticate={auth.user.uid!== undefined} />}>
-            <Route path="Login" element={<Login />} />
+          <Route>
+            <Route index element={<Home />} />
             <Route path="home" element={<Home />} />
             <Route path="Apoyanos" element={<Apoyanos />} />
             <Route path="History" element={<History />} />
             <Route path="Contacto" element={<Contacto />} />
             <Route path="Swiper" element={<Slider />} />
-          </Route>
-          <Route element={<PrivatedRoutes isAuthenticate={auth.user.uid === undefined} />}>
             <Route path="Eventos" element={<Eventos />} />
-            <Route path="Talleres" element={<Talleres />} />
-            <Route path="Noticias" element={<Noticias />} />
-            <Route path="Admin" element={<AdminPanel />} />
+              <Route path="Talleres" element={<Talleres />} />
+              <Route path="Noticias" element={<Noticias />} />
+
+            <Route element={<PublicRoutes isAuthenticate={isAuthenticate} />}>
+              <Route path="Login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Route>
+            <Route element={<PrivatedRoutes isAuthenticate={isAuthenticate} />}>
+              
+              
+              {user && user?.rol === "admin" ? (
+                <Route path="admin" element={<AdminPanel />} />
+              ) : (
+                <></>
+              )}
+            </Route>
           </Route>
-          <Route path="register" element={<Register />} />
         </Route>
       </Routes>
     </>
